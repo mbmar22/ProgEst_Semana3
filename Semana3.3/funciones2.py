6355ab9cc390035251c1248882f09b15d03ce294
Maria <mbmartinezo@uamv.edu.ni>
2026-09-04 08:06:11 -0600

    Funciones 2

---
+++ Semana3.3/funciones2.py
@@ -0,0 +1,12 @@
+# Sumar 2 números
+def sumar(numero1, numero2):
+    return numero1 + numero2
+
+num1 = float(input("Ingrese un número: "))
+num2 = float(input("Ingrese un número: "))
+
+print(sumar(num1, num2))
+
+sumar = sumar(15, 17)
+
+print(sumar)
